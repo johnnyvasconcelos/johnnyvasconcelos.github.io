57d0ec5d632aa2c2b0d547d5e2a9cd96c9fcd664
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-05-25 22:25:15 -0300

    ajustes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,27 @@
 {
-  const darkMode = document.querySelector(".dark-mode");
-  darkMode.addEventListener("click", () => {
-    darkMode.classList.toggle("white-mode");
+  const darkBtn = document.querySelector(".menu__dark");
+  const letters = document.querySelector(".thin");
+  darkBtn.addEventListener("click", () => {
+    darkBtn.classList.toggle("white-mode");
   });
+  const phrase = "performance e SEO";
+  const phraseArray = phrase.split("");
+  letters.textContent = "";
+  let i = 0;
+  const interval = setInterval(() => {
+    if (i < phraseArray.length) {
+      letters.textContent += phraseArray[i];
+      i++;
+    } else {
+      clearInterval(interval);
+      pipeInvoc();
+    }
+  }, 200);
+  function pipeInvoc() {
+    letters.innerHTML += "<span class='pipe'>|</span>";
+    const pipe = document.querySelector(".pipe");
+    setInterval(() => {
+      pipe.classList.toggle("opacity");
+    }, 800);
+  }
 }
